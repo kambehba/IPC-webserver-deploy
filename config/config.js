bdc5ebf5a35c56a51129464d579d1f63e8d6b7b2
kambehba <kamparsen@yahoo.com>
2017-08-31 15:46:09 -0400

    init
    init

---
+++ config/config.js
@@ -0,0 +1,14 @@
+var configValues = require('./config');
+module.exports = {
+
+getDbConnectionString:function(){
+    
+        //return 'mongodb://' + configValues.username +':' +configValues.password+'@ds159747.mlab.com:59747/task-manager';
+        //return 'mongodb://localhost/InternetControlDatabase';
+        //return 'output-server:k8084164@ds123050.mlab.com:23050/app-data';
+        return 'mongodb://kam:k8084164@ds127300.mlab.com:27300/heroku_16jnb8q4';
+        
+        //return 'mongodb://output-server:k8084164@ds123050.mlab.com:23050/app-data';
+
+    }
+}
---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require('firebase-functions');
+var express = require('express');
+var firebase = require('firebase')
+var app = express();
+//var mongoose = require('mongoose');
+var config = require('./config/config');
+var apiController = require('./controllers/outputController')
+var port = process.env.PORT || 3000;
+//var port = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 3000;
+//var setupController = require('./controllers/setupController');
+app.use('/assets',express.static(__dirname+'/public'));
+
+//this is added to resolve cross origin problem
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+ //res.send("WELdsdsccccccccdCOME");
+  next();
+});
+
+
+app.set('view engine','ejs');
+
+console.log('1111115555551111111111');
+
+
+ var config = {
+    apiKey: "AIzaSyDYiYRMlAZpEXya9aVUhv9cpJwJL4Oz7gM",
+    authDomain: "dazzling-torch-8270.firebaseapp.com",
+    databaseURL: "https://dazzling-torch-8270.firebaseio.com",
+    projectId: "dazzling-torch-8270",
+    storageBucket: "dazzling-torch-8270.appspot.com",
+    messagingSenderId: "935228019520"
+  };
+  firebase.initializeApp(config);
+
+console.log('firebase connected...');
+//mongoose.connect(config.getDbConnectionString());
+//mongoose.connect('output-server:k8084164@ds123050.mlab.com:23050/app-data');
+//console.log(config.getDbConnectionString());
+/*mongoose.connection
+    .once('open', () => console.log('Good to go!'))
+    .on('error',(error) =>{consol.warn('Warning',error);
+});*/
+
+//setupController(app);
+apiController(app);
+app.listen(port);
